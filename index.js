a890c0e62119e66c6345e3af63b38e8ea6cc2cd8
Kali Deming <kalideming@icloud.com>
2022-11-02 15:38:27 -0600

    commit tree 6f86b481c767b27b6bacb5364ccbbd5f6596a1ff

--- index.js
+++ index.js
@@ -1,1 +1,96 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyAppendCat(catName) {
+    return cats.push(catName);
+};
+destructivelyAppendCat('Ralph');
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyPrependCat(catName) {
+    return cats.unshift(catName);
+};
+destructivelyPrependCat('Bob');
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+  };
+  destructivelyRemoveLastCat();
+
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+  };
+
+  destructivelyRemoveFirstCat();
+
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function appendCat(catName) {
+    const allCats = [...cats, catName];
+    return allCats;
+  };
+  appendCat('Broom');
+
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function prependCat(catName) {
+    const moreCats = [catName, ...cats];
+    return moreCats;
+  };
+  prependCat('Arnold');
+
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function removeLastCat() {
+    const wowCats = cats.slice(0, cats.length - 1);
+    return wowCats;
+  };
+  removeLastCat();
+
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+  function removeFirstCat() {
+    const holyFuckCats = cats.slice(1);
+    return holyFuckCats; 
+  };
+  removeFirstCat();
